df2b2fc46f31b3157f1d7892635f92251dfafee8
Nicolas HODEE <accounts@nicolashodee.com>
2020-06-09 21:28:26 +0100

    created user and producer mailers

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#settings for the mailer
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_LOGIN'],
+  :password => ENV['SENDGRID_PWD'],
+  :domain => 'nicolashodee.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
---
+++ test/mailers/previews/producer_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/producer_mailer
+class ProducerMailerPreview < ActionMailer::Preview
+
+end
