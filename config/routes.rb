40b6ca0caec790b211fee23b0723e430018ab358
Titpioupiou <carriere.aurore@orange.fr>
2020-06-11 11:03:12 +0200

    routes modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :users, :path => "My user profile"
-  devise_for :producers
-  resources :producers, :path => "My producer profile"
+  devise_for :users, :path => "my_user_profile"
+  devise_for :producers, :path => "my_producer_profile"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 end
