7e33c278c1248168e8eae3e03686b418bca69147
JordanT <jo.tournerie@gmail.com>
2020-06-12 09:55:25 +0200

    front contact v1, little changes in design

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, :path => "My user profile"
+  resources :users, :path => "My_user_profile"
   devise_for :producers
-  resources :producers, :path => "My producer profile"
+  resources :producers, :path => "My_producer_profile"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+  get '/contact', to: 'static_pages#contact'
 end
