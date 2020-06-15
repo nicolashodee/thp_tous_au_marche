0fd802b228d3dcc777fdbd7a7316eb5cbc17c275
Homarv <pierratmarvin3@gmail.com>
2020-06-15 15:34:23 +0200

    add map to index_producers with leaflet gem

---
+++ config/initializers/leaflet.rb
@@ -0,0 +1,4 @@
+Leaflet.tile_layer = '//{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png'
+# You can also use any other tile layer here if you don't want to use Cloudmade - see http://leafletjs.com/reference.html#tilelayer for more
+Leaflet.attribution = "Your attribution statement"
+Leaflet.max_zoom = 18
