279f2d5fe6ad708f545c15e817e05dede3025f1b
Nicolas HODEE <accounts@nicolashodee.com>
2020-06-16 13:26:56 +0100

    trying to integrate recaptcha with devise

--- config/initializers/recaptcha.rb
+++
@@ -1,5 +0,0 @@
-Recaptcha.configure do |config|
-  config.site_key  = Rails.application.secrets.recaptcha_site_key
-  config.secret_key = Rails.application.secrets.recaptcha_secret_key
-  config.api_version = 'v3'
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root 'welcome#home'
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   resources :users, :path => "mon_profil"
   devise_for :producers
   resources :producers, :path => "profil_producteur"
   resources :comments
   get '/contact', to: 'static_pages#contact'
-
 end
