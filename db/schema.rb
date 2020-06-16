1e406e8de59bb618d8c57e862a9e164fc12024fd
cfacco1 <facco.clement@gmail.com>
2020-06-16 17:30:26 +0200

    back admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_15_130903) do
+ActiveRecord::Schema.define(version: 2020_06_16_152504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 2020_06_15_130903) do
     t.string "first_name"
     t.string "last_name"
     t.string "phone_number"
+    t.boolean "is_admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
