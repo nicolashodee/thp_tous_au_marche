f9d503f4c063f51a1e039f6b4b67d2afe53d9244
Nicolas HODEE <accounts@nicolashodee.com>
2020-06-17 15:41:49 +0100

    contact form installed, testing now on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,4 +98,18 @@ Rails.application.configure do
   # config.action_mailer.delivery_method = :smtp
   # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   #### end of Mailer ####
+
+  config.action_mailer.default_url_options = { host: 'https://your app.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'heroku.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+  }
+
 end
