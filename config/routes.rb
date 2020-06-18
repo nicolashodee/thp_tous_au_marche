3e11a54b5488518e23ed9478c9c60185b29a6c54
Titpioupiou <carriere.aurore@orange.fr>
2020-06-19 00:32:59 +0200

    not able to connect as user & producer at the same time

--- config/routes.rb
+++ config/routes.rb
@@ -26,12 +26,12 @@ Rails.application.routes.draw do
     resources :avatars, only: [:create]
     resources :images, only: [:create]
   end
-  
-  namespace :admin do
-    resources :producers, :comments, :users
-  end
 
   resources :users, only: [:show] do
     resources :avatar_users, only: [:create]
+  end
+
+  namespace :admin do
+    resources :producers, :comments, :users
   end
 end
