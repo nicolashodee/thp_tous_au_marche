a87464211298d7a2a07affa3d42a512a57d863f1
Homarv <pierratmarvin3@gmail.com>
2020-06-18 15:21:29 +0200

    add migration table to latitude/longitude

---
+++ db/migrate/20200618132039_add_latitude_longitude_to_producers.rb
@@ -0,0 +1,6 @@
+class AddLatitudeLongitudeToProducers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :producers, :latitude, :decimal
+    add_column :producers, :longitude, :decimal
+  end
+end
