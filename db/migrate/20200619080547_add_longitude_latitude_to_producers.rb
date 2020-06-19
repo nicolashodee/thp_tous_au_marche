ee8af7ece4f3a4e380d7e1cc7c324e3f7145dc19
Homarv <pierratmarvin3@gmail.com>
2020-06-19 10:13:30 +0200

    problem with migration

--- db/migrate/20200619080547_add_longitude_latitude_to_producers.rb
+++
@@ -1,6 +0,0 @@
-class AddLongitudeLatitudeToProducers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :producers, :longitude, :decimal
-    add_column :producers, :latitude, :decimal
-  end
-end
